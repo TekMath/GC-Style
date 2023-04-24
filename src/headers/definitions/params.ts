90588b47ca27a0828e7e00f1bc23de50b4a4e6fc
TekMath <dokuhebi.game@gmail.com>
2023-04-25 01:38:03 +0200

    🚧 Added unused params method

--- src/headers/definitions/params.ts
+++ src/headers/definitions/params.ts
@@ -1,19 +1,47 @@
 import { logs } from "../../utils/logs"
 import { CodeError } from "@/index";
+import { getLines } from "../../../src/parse/content";
+
+/**
+ * Check if a definition/macro has params
+ * @param lineArray Array of this line split by space
+ * @returns True if there are one param or more, else False
+ */
+function hasParams(lineArray: string[]): boolean {
+    const definition: string = lineArray[1];
+    const definitionSplit: string[] = definition.split("(");
+
+    if (definitionSplit.length <= 1)
+        return false;
+    return true;
+}
+
+function unusedParams(fileContent: string): CodeError | undefined {
+    const lines: string[] = getLines(fileContent);
+
+    for (const line of lines) {
+        const lineArray: string[] = line.split(" ");
+
+        if (lineArray[0] != "#define")
+            continue;
+        if (hasParams(lineArray))
+            console.log(line);
+    }
+    return undefined;
+}
 
 /**
  * Check if there are coding-style errors in macro/definition params
- * @param file Header file that must be check
+ * @param fileContent Content of a header file
  * @returns Array of errors
  */
-function definitionsParams(file: string): CodeError[] {
-    const fileExtention = file.split(".");
+function definitionsParams(fileContent: string): CodeError[] {
     let errorsArray: CodeError[] = [];
+    let error: CodeError | undefined;
 
-    if (fileExtention.length < 2 || fileExtention.at(-1) != "h") {
-        logs.error("File extention", "The file must be a header");
-        return [];
-    }
+    error = unusedParams(fileContent);
+    if (error)
+        errorsArray.push(error);
     return errorsArray;
 }
 
